77422c8654ef6f52cdb5c8c6106201d8d3a4902f
Irfanul Hasan <irfan.dev.bd@gmail.com>
2025-12-10 18:15:12 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+
+string jsonContent = System.IO.File.ReadAllText("H:\\Github\\Converter\\TreatyXml\\treaty.json");
+
+XDocument xmlDoc = TreatyConverter.ConvertTreatyJsonToXml(jsonContent);
+
+// Save with proper formatting
+var settings = new XmlWriterSettings
+{
+    Indent = true,
+    IndentChars = "  ",
+    OmitXmlDeclaration = false,
+    Encoding = System.Text.Encoding.UTF8
+};
+
+using (var writer = XmlWriter.Create("H:\\Github\\Converter\\TreatyXml\\output.xml", settings))
+{
+    xmlDoc.Save(writer);
+}
+
+Console.WriteLine("Conversion completed!");
